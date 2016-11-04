a832d3e3e00fc2e37eca8cd58a74c107949cda91
Mishiranu <fukurou.mishiranu@gmail.com>
2016-11-04 18:08:31 +0300

    Fix StringUtils call

--- src/com/mishiranu/dashchan/chan/nowere/NowerePostsParser.java
+++ src/com/mishiranu/dashchan/chan/nowere/NowerePostsParser.java
@@ -287,7 +287,7 @@ public class NowerePostsParser implements GroupParser.Callback
 			}
 			case EXPECT_SUBJECT:
 			{
-				mPost.setSubject(StringUtils.emptyIfNull(StringUtils.clearHtml(text).trim()));
+				mPost.setSubject(StringUtils.nullIfEmpty(StringUtils.clearHtml(text).trim()));
 				break;
 			}
 			case EXPECT_NAME:
@@ -300,12 +300,12 @@ public class NowerePostsParser implements GroupParser.Callback
 					else mPost.setEmail(email);
 					text = matcher.group(2);
 				}
-				mPost.setName(StringUtils.emptyIfNull(StringUtils.clearHtml(text).trim()));
+				mPost.setName(StringUtils.nullIfEmpty(StringUtils.clearHtml(text).trim()));
 				break;
 			}
 			case EXPECT_TRIPCODE:
 			{
-				mPost.setTripcode(StringUtils.emptyIfNull(StringUtils.clearHtml(text).trim()));
+				mPost.setTripcode(StringUtils.nullIfEmpty(StringUtils.clearHtml(text).trim()));
 				break;
 			}
 			case EXPECT_COMMENT:
